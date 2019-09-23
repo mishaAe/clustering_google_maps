79523627bcf367c08f422e0af9dd37438d66ca75
Rinat <ultramarinat@gmail.com>
2019-09-23 12:40:31 +0300

    Get state as method

--- lib/src/selectable_item.dart
+++ lib/src/selectable_item.dart
@@ -1,16 +1,17 @@
-import 'package:geohash/geohash.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../clustering_google_maps.dart';
 
 class StateItem extends ClusterItem {
-  dynamic state;
-
   Future<BitmapDescriptor> getBitmapDescriptor(
       AggregationSetup aggregationSetup) async {
-    var icon = aggregationSetup.iconData[state.toString()];
+    var icon = aggregationSetup.iconData[getState()];
     if (icon != null)
       return icon;
     return BitmapDescriptor.defaultMarker;
+  }
+
+  String getState() {
+    return '';
   }
 }
